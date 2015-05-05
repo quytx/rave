f058b4a51c33584ea369cdb4db00fe31e4302350
quytx1102 <quytx1102@gmail.com>
2015-05-05 17:48:58 -0500

    tmp folder on heroku

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -4,7 +4,7 @@ require 'securerandom'
 module PhotosHelper
   def saveImg(data)
     img = Magick::Image.read_inline(data)[0]
-    path = 'public/uploads/' + SecureRandom.hex(8) + '.' + img.format.downcase
+    path = "#{Rails.root}/tmp/" + SecureRandom.hex(8) + '.' + img.format.downcase
     img.write(path)
     path
   end

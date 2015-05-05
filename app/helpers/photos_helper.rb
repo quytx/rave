ca7743d55f881850bdbe5c2550e02923db81b722
quytx1102 <quytx1102@gmail.com>
2015-05-05 17:22:31 -0500

    fixing"

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -3,8 +3,11 @@ require 'base64'
 require 'securerandom'
 module PhotosHelper
   def saveImg(data)
+    puts "11111111"
     img = Magick::Image.read_inline(data)[0]
+    puts "2222222222"
     path = 'public/uploads/' + SecureRandom.hex(8) + '.' + img.format.downcase
+    puts "33333333 writing"
     img.write(path)
     path
   end

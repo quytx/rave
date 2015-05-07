e06760e45830f80ccab4e50db6a45c70e3727511
quytx1102 <quytx1102@gmail.com>
2015-05-06 20:56:45 -0500

    events

--- db/migrate/20150502000923_create_events.rb
+++ db/migrate/20150502000923_create_events.rb
@@ -3,15 +3,11 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.integer :user_id
       t.string :name
-      t.string :description
       t.string :location
+      t.string :description
       t.datetime :start_time
       t.datetime :end_time
       t.timestamps
     end
-  end
-
-  def change
-    add_column :description, :string
   end
 end

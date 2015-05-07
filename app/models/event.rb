0461504ac73cdf3d294247f2fd65c3167cf33701
quytx1102 <quytx1102@gmail.com>
2015-05-07 11:22:29 -0500

    guests count

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,12 @@ class Event < ActiveRecord::Base
   has_many :photos
   has_many :event_participants
   has_many :participants, through: :event_participants, source: :user
+
+  # def guest_count
+  #   self.participants.all.count
+  # end
+
+  # def as_json(options={})
+  #   super.as_json(options).merge({:participant_count => guest_count})
+  # end
 end
